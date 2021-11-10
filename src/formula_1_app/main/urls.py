7e01124f707d7c11f6a2a39d3fcdcf1d25e687bd
Tema Sarkisov <sarkisov@artlebedev.ru>
2021-11-10 18:46:55 +0300

    Add admin/standart rights

--- src/formula_1_app/main/urls.py
+++ src/formula_1_app/main/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('search/', search, name='search'),
+    path('user_settings/', user_settings, name='user_settings'),
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='sign_up')
 ]
--- src/formula_1_app/main/views.py
+++ src/formula_1_app/main/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import check_password
 from django.views.generic import TemplateView
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -11,9 +12,11 @@ import requests
 
 
 TEMPLATE_LOGIN = 'main/login.html'
-TEMPLATE_REGISTER = 'main/sign_up.html'
+TEMPLATE_USER_SETTINGS = 'main/user_settings.html'
+TEMPLATE_SIGN_UP = 'main/sign_up.html'
 TEMPLATE_SEARCH = 'main/search.html'
 
+#------------------------------------------------------------------------------
 
 class LoginView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
@@ -29,7 +32,8 @@ class LoginView(TemplateView):
             else:
                 context['error'] = 'Incorrect login or password'
         return render(request, TEMPLATE_LOGIN, context)
-    
+
+#------------------------------------------------------------------------------  
 
 class SignUpView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
@@ -43,8 +47,9 @@ class SignUpView(TemplateView):
                 User.objects.create_user(username, email, password)
                 return redirect(reverse("login"))
 
-        return render(request, TEMPLATE_REGISTER)
+        return render(request, TEMPLATE_SIGN_UP)
 
+#------------------------------------------------------------------------------
 
 def search(request):
     if request.user.is_authenticated:
@@ -85,20 +90,15 @@ def search(request):
                                         ])
                 print(result_list[0])
 
-                return render(request, 'main/search.html', {'result_list': result_list})                  
+                return render(request, TEMPLATE_SEARCH, {'result_list': result_list})                  
 
     if request.user.is_authenticated:
-        return render(request, 'main/search.html')
+        return render(request, TEMPLATE_SEARCH)
     else:
-        return render(request, 'main/search.html', {'error': 'Please login to use Formula 1 Analyser'})
-
-def result(request):
-    return render(request, 'main/result.html', {'result_list': {},
-                                               })
+        return render(request, TEMPLATE_SEARCH, {'error': 'User was not authenticated'})
 
 def value_to_cut_value(value: str) -> str:
     return value.replace('_', '+').replace('-', '\-')
-
 
 def links_values_to_request(links_list: list, values_list: list) -> str:
     if len(links_list) == len(values_list):
@@ -113,70 +113,53 @@ def links_values_to_request(links_list: list, values_list: list) -> str:
     
     return ''
 
+#------------------------------------------------------------------------------
 
-'''class SearchView():
-    def search(self, request):
-        if request.method == 'POST':
-            values_dict = request.POST.dict()
-            values_dict.pop('csrfmiddlewaretoken', None)
+def user_settings(request):
+    username = request.user.username
+    user = User.objects.get(username__exact=username)
+    password = user.password
+    email = user.email
+    is_superuser = user.is_superuser
 
-            with open (f'{settings.BASE_DIR}/main/static/main/links/links.json') as links_file:
-                links_dict = json.load(links_file)
+    users = User.objects.all()
+    users_info_list = []
+    for user in users:
+        users_info_list.append([user.id, user.is_superuser, user.username, user.email])
 
-            links_values_dict = {}
-            links_list = []
-            values_list = []
-            for _, key in enumerate(values_dict):
-                if (values_dict[key] != ''):
-                    links_list.append(links_dict[key])
-                    values_list.append(values_dict[key])
-                    links_values_dict[links_dict[key]] = values_dict[key]
+    if request.method == 'POST':
+        if not is_superuser:
+            new_email = request.POST['value_new_email_to_change']
+            password_to_confirm = request.POST['value_password_to_change']
 
-            #print(links_values_dict)
+            if new_email != email and check_password(password_to_confirm, password):
+                user.email = new_email
+                user.save()
+                
+                return render(request, TEMPLATE_USER_SETTINGS, {'username': user.username, 'email': user.email, 'is_superuser': is_superuser})
 
-            if links_values_dict:
-                cube_request = self.__links_values_to_request(links_list, values_list)
-                result_dicts_list = requests.get(cube_request).json()
-                print(result_dicts_list[0])
-                result_list = []
-                for result_dict in result_dicts_list:
-                    result_list.append([result_dict['__fact_key__'],
-                                        result_dict['driver.forename'],
-                                        result_dict['driver.surname'],
-                                        result_dict['race.name'],
-                                        result_dict['race.date'],
-                                        result_dict['number'],
-                                        result_dict['grid'],
-                                        result_dict['position'],
-                                        result_dict['position_order'],
-                                        result_dict['laps'],
-                                        result_dict['points'],
-                                        result_dict['fastest_lap_speed'],
-                                        ])
-                print(result_list[0])
+        if is_superuser:
+            username_to_delete = request.POST['value_username_to_delete']
+            password_to_delete = request.POST['value_password_to_delete']
 
-                return render(request, 'main/search.html', {'result_list': result_list, 
-                                                        })                                         
-
-        return render(request, 'main/search.html', {'result_list': {},
-                                                })
-    def result(request):
-        return render(request, 'main/result.html', {'result_list': {},
-                                                })
-
-    def __value_to_cut_value(self, value: str) -> str:
-        return value.replace('_', '+').replace('-', '\-')
-
-
-    def __links_values_to_request(self, links_list: list, values_list: list) -> str:
-        if len(links_list) == len(values_list):
-            cube_request_cut = ''
-            for i in range(len(links_list)):
-                if i == 0:
-                    cube_request_cut += f'{links_list[i]}:{self.__value_to_cut_value(values_list[i])}'
-                else:
-                    cube_request_cut += f'|{links_list[i]}:{self.__value_to_cut_value(values_list[i])}'
+            try:
+                user_to_delete = User.objects.get(username__exact=username_to_delete)
+            except:
+                return render(request, TEMPLATE_USER_SETTINGS, {'username': user.username, 'email': user.email, 'is_superuser': is_superuser, 'users_info_list': users_info_list})
             
-            return f'http://localhost:5000/cube/result/facts?cut={cube_request_cut}'
-        
-        return '' '''
+            if user_to_delete and check_password(password_to_delete, password):
+                user_to_delete.delete()
+                users = User.objects.all()
+                users_info_list = []
+                for user in users:
+                    users_info_list.append([user.id, user.is_superuser, user.username, user.email])
+                
+                return render(request, TEMPLATE_USER_SETTINGS, {'username': user.username, 'email': user.email, 'is_superuser': is_superuser, 'users_info_list': users_info_list})
+    
+    if not is_superuser:
+        return render(request, TEMPLATE_USER_SETTINGS, {'username': username, 'email': email, 'is_superuser': is_superuser})
+    if is_superuser:
+        return render(request, TEMPLATE_USER_SETTINGS, {'username': username, 'email': email, 'is_superuser': is_superuser, 'users_info_list': users_info_list})
+    
+    return render(request, TEMPLATE_USER_SETTINGS, {'error': 'Something went wrong :('})
+
